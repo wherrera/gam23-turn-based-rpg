442e374d9862fa797f957e1502f84f25e9b3b2ee
William Herrera <william.herrera@rccd.edu>
2019-03-18 21:56:30 -0700

    added player movement and animation

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 1F;
+
+    public SpriteRenderer walk;
+
+    void Update()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        if(Horizontal > 0f)
+        {
+            walk.flipX = false;
+        }
+        else if (Horizontal < 0f)
+        {
+            walk.flipX = true;
+        }
+
+        transform.position += (new Vector3(Horizontal, Vertical, 0f) * speed) * Time.deltaTime;
+    }
+}
